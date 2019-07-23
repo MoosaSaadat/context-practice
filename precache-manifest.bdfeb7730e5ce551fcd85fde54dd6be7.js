0d704b1358abb27c436385dab5c8961a8881f0d4
Muhammad Musa <moosasaadat99@gmail.com>
2019-07-23 22:40:37 +0500

    Updates

---
+++ precache-manifest.bdfeb7730e5ce551fcd85fde54dd6be7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c04780b2ab7bd59f544726b824bff1a",
+    "url": "/context-practice/index.html"
+  },
+  {
+    "revision": "a9e9ea2ffac3c6d5b5e3",
+    "url": "/context-practice/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1bdd30901874695b73b3",
+    "url": "/context-practice/static/js/2.3c93a35a.chunk.js"
+  },
+  {
+    "revision": "a9e9ea2ffac3c6d5b5e3",
+    "url": "/context-practice/static/js/main.4629287e.chunk.js"
+  },
+  {
+    "revision": "c3984eb0f6fcd8a8c1c8",
+    "url": "/context-practice/static/js/runtime~main.cc647cc2.js"
+  }
+]);
